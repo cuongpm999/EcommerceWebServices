0492619ca69406e748907fcaca3340548e951b90
Cuong Pham <phamcuongth2000@gmail.com>
2021-10-22 16:37:42 +0700

    them rest controller

---
+++ src/main/java/vn/ptit/controllers/admin/electronics/AdminManufacturerController.java
@@ -0,0 +1,39 @@
+package vn.ptit.controllers.admin.electronics;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.ptit.entities.electronics.Laptop;
+import vn.ptit.entities.electronics.Manufacturer;
+import vn.ptit.repositories.electronics.ManufacturerRepository;
+
+@RestController
+@RequestMapping("/rest/api/manufacturer")
+public class AdminManufacturerController {
+	@Autowired ManufacturerRepository manufacturerRepository;
+	
+	@GetMapping(value = "/find-all")
+	public List<Manufacturer> findAll(ModelMap model, HttpServletRequest req, HttpServletResponse resp) {
+
+		return manufacturerRepository.findAll();
+	}
+	
+	@PostMapping(value = "/insert")
+	public Manufacturer insert(@RequestBody Manufacturer manufacturer, ModelMap model, HttpServletRequest req, HttpServletResponse resp) {
+
+		return manufacturerRepository.save(manufacturer);
+	}
+	
+	
+
+}
