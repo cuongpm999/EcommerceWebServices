ea1552f15a15a79b398692e424fa2661309ed98e
Cuong Pham <phamcuongth2000@gmail.com>
2021-10-25 11:15:12 +0700

    xong book item

--- src/main/java/vn/ptit/repositories/electronics/ElectronicsItemRepository.java
+++ src/main/java/vn/ptit/repositories/electronics/ElectronicsItemRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import vn.ptit.entities.electronics.ElectronicsItem;
 
 @Repository
-public interface ElectronicsItemRepository extends JpaRepository<ElectronicsItem, Integer>{
+public interface ElectronicsItemRepository extends JpaRepository<ElectronicsItem, String>{
 
 }
