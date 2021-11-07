b556727af98d5a178c7507bab99c33183cb9740f
ThanhTuyen00 <tuyentitxdc@gmail.com>
2021-11-07 23:28:21 +0700

    order

---
+++ src/main/java/vn/ptit/entities/order/LineBookItem.java
@@ -0,0 +1,69 @@
+package vn.ptit.entities.order;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import vn.ptit.entities.book.BookItem;
+
+@Entity
+@Table(name = "LineBookItem")
+public class LineBookItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+	
+	@Column(name = "Quanity")
+	private int quanity;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "BookItemBarCode")
+	private BookItem bookItem;
+	
+	@JsonBackReference
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "CartID")
+	private ShoppingCart shoppingCart;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getQuanity() {
+		return quanity;
+	}
+
+	public void setQuanity(int quanity) {
+		this.quanity = quanity;
+	}
+
+	public BookItem getBookItem() {
+		return bookItem;
+	}
+
+	public void setBookItem(BookItem bookItem) {
+		this.bookItem = bookItem;
+	}
+
+	public ShoppingCart getShoppingCart() {
+		return shoppingCart;
+	}
+
+	public void setShoppingCart(ShoppingCart shoppingCart) {
+		this.shoppingCart = shoppingCart;
+	}
+	
+}
